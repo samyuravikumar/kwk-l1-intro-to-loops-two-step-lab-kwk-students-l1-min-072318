a887c3e25732891447ffa26a24b38d51dc5b5f94
samyuravikumar <samyuktha321@gmail.com>
2018-07-24 19:43:19 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -12,11 +12,12 @@ def first_steps
   # "Right foot back", and "Stop", with sleep after each
 end
 
-first_steps
+# first_steps
 
 
 def a_few_more_steps
   # Write a loop that outputs the first two sets of steps in the Two-Step
+  
 end
 
 def how_many_steps?
